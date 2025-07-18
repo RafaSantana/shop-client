54952adc92e9747969261297ce07bf9aa6f1294e
Rafael Santana <rafaelsantana.tech@gmail.com>
2025-07-18 19:17:50 -0300

    feat: define main routes of the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home').then(m => m.HomeComponent),
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./components/client-product-list/client-product-list').then(
+        m => m.ClientProductListComponent,
+      ),
+  },
+  {
+    path: 'products-server',
+    loadComponent: () =>
+      import('./components/server-product-list/server-product-list').then(
+        m => m.ServerProductListComponent,
+      ),
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () =>
+      import('./components/product-detail/product-detail').then(m => m.ProductDetailComponent),
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./components/cart/cart').then(m => m.CartComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
+];
