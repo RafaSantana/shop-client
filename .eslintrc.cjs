417476d2f41b8218481895b75f87dd21a7b6dd7a
Rafael Santana <rafaelsantana.tech@gmail.com>
2025-07-18 12:13:21 -0300

    refactor: update ESLint configuration to use module.exports and enhance TypeScript rules
    chore: update package-lock.json to reflect dependency changes and remove unused packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,34 @@
-export const root = true;
-export const ignorePatterns = ['projects/**/*'];
-export const overrides = [
-  {
-    files: ['*.ts'],
-    extends: [
-      'plugin:@angular-eslint/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-    ],
-    parserOptions: {
-      project: ['./tsconfig.json'],
-      createDefaultProgram: true,
+module.exports = {
+  root: true,
+  ignorePatterns: ['projects/**/*'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: [
+        'plugin:@angular-eslint/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        createDefaultProgram: true,
+      },
+      rules: {
+        // Suas regras personalizadas
+        '@typescript-eslint/explicit-function-return-type': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-floating-promises': 'warn',
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+      },
     },
-    rules: {
-      // Suas regras personalizadas
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
+    {
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      plugins: ['@typescript-eslint', '@angular-eslint'],
+      rules: {
+        // Regras para templates
+      },
     },
-  },
-  {
-    files: ['*.html'],
-    extends: ['plugin:@angular-eslint/template/recommended'],
-    rules: {
-      // Regras para templates
-    },
-  },
-];
+  ],
+};
